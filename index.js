87c7e16754807221971441d5f68c1fa5f605ab36
Drew Overcash <drewovercash@gmail.com>
2017-10-21 19:55:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,11 +2,12 @@
 var greeting = "Hello, everybody!";
 ```
 
-var specialGuest = "Neil deGrasse Tyson"
-
+```js
+var specialGuest = "Neil deGrasse Tyson";
 var greetSpecialGuest = 'Hello, ${specialGuest}!';
+```
 
+```js
 var topic = "space.";
-
 var conversation = 'Let's talk about ${topic}';
 ```
